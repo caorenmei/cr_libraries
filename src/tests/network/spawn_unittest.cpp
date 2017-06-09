74e46b13ccdf0cdc72510830a4df0804b68c0b7d
caorenmei <caorenmei@outlook.com>
2017-06-10 03:07:11 +0800

    完善spawn测试

--- src/tests/network/spawn_unittest.cpp
+++ src/tests/network/spawn_unittest.cpp
@@ -9,6 +9,7 @@ BOOST_AUTO_TEST_SUITE(cr_network_spawn)
 BOOST_AUTO_TEST_CASE(spawn)
 {
     boost::asio::io_service ioService;
+    std::size_t loopCount = 0;
     cr::network::spawn(ioService, [&](cr::network::Coroutine coro)
     {
         for (std::size_t i = 0; i != 10; ++i)
@@ -18,9 +19,11 @@ BOOST_AUTO_TEST_CASE(spawn)
                 coro.resume();
             });
             coro.yield();
+            loopCount = loopCount + 1;
         }
     });
     ioService.run();
+    BOOST_CHECK_EQUAL(loopCount, 10);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
