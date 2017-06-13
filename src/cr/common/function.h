9d2bc60105919d84efc0a05c358ba4e0b2c37d47
caorenmei <caorenmei@outlook.com>
2017-06-13 21:37:55 +0800

    shift定义简化

--- src/cr/common/function.h
+++ src/cr/common/function.h
@@ -8,29 +8,23 @@ namespace cr
 {
     namespace fun
     {
-
         template <typename Functor, typename... Args>
-        auto shiftAux(std::integral_constant<std::size_t, 0>, Functor&& call, Args&&... args)
+        auto shift(std::integral_constant<std::size_t, 0>, Functor& call, Args&&... args)
         {
             return call(std::forward<Args>(args)...);
         }
 
-        template <typename Constant, typename Functor, typename T, typename... Args>
-        auto shiftAux(Constant, Functor&& call, T&& arg, Args&&... args)
-        {
-            return shiftAux(std::integral_constant<std::size_t, Constant::value - 1>(), std::forward<Functor>(call), std::forward<Args>(args)...);
-        }
-
         /**
          * 移动函数参数
+         * @param right 参数移动位数
          * @param call 处理器.
          * @param args 参数列表.
          * @return call().
          */
-        template <std::size_t N, typename Functor, typename... Args>
-        auto shift(Functor&& call, Args&&... args)
+        template <std::size_t N, typename Functor, typename T, typename... Args>
+        auto shift(std::integral_constant<std::size_t, N> right, Functor& call, T&& arg, Args&&... args)
         {
-            return shiftAux(std::integral_constant<std::size_t, N>(), std::forward<Functor>(call), std::forward<Args>(args)...);
+            return shift(std::integral_constant<std::size_t, N - 1>(), call, std::forward<Args>(args)...);
         }
     }
 }
