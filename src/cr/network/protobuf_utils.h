98f6f7c29347f1e34a096bf7e0b9f6de75838b5a
caorenmei <caorenmei@outlook.com>
2017-06-17 23:18:20 +0800

    getProtobufMessageFromName方法添加日志

--- src/cr/network/protobuf_utils.h
+++ src/cr/network/protobuf_utils.h
@@ -1,4 +1,4 @@
-#ifndef CR_NETWORK_PROTOBUF_UTILS_H_
+﻿#ifndef CR_NETWORK_PROTOBUF_UTILS_H_
 #define CR_NETWORK_PROTOBUF_UTILS_H_
 
 #include <google/protobuf/message.h>
@@ -7,6 +7,11 @@ namespace cr
 {
     namespace network
     {
+        /**
+         * 通过消息名字动态构造消息
+         * @param typeName 消息名字
+         * @return protobuf消息，nullptr 构造失败
+         */
         google::protobuf::Message* getProtobufMessageFromName(const std::string& typeName);
     }
 }
