9d9a2d2276ed25ab10d65b22abf17c0699e7c6fc
caorenmei <caorenmei@outlook.com>
2017-06-27 21:36:52 +0800

    增加跟随者状态

---
+++ src/cr/raft/follower.cpp
@@ -0,0 +1,37 @@
+#include <cr/raft/follower.h>
+
+#include <cr/common/assert.h>
+#include <cr/raft/raft_engine.h>
+
+namespace cr
+{
+    namespace raft
+    {
+        Follower::Follower(RaftEngine& engine)
+            : RaftState(engine)
+        {}
+
+        Follower::~Follower()
+        {}
+
+        void Follower::onEnter(std::shared_ptr<RaftState> prevState)
+        {
+            
+        }
+
+        void Follower::onLeave()
+        {
+
+        }
+
+        std::int64_t Follower::update(std::int64_t nowTime, std::vector<RaftMsgPtr>& outMessages)
+        {
+            return nowTime;
+        }
+
+        std::int64_t Follower::update(std::int64_t nowTime, RaftMsgPtr inMessage, std::vector<RaftMsgPtr>& outMessages)
+        {
+            return nowTime;
+        }
+    }
+}
