43e58e2bd07e21f77c8a82a0dec50515e9ff61f1
caorenmei <caorenmei@outlook.com>
2017-07-09 13:15:29 +0800

    rocks编译脚本

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -2,6 +2,6 @@ if (NOT _CR_MSVC AND WIN32)
     set(_CR_MSVC 1)
 endif()
 
-foreach(libName boost cpp-netlib eluna lua mongodb mysql protobuf rapidxml)
+foreach(libName boost cpp-netlib lua mysql protobuf rapidxml rocksdb)
     execute_process(COMMAND cmake -D_CR_MSVC=${_CR_MSVC} -P "${CMAKE_CURRENT_SOURCE_DIR}/${libName}.cmake" WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
 endforeach()
