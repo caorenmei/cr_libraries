fef8de15b277fdad23657971eef98a3d191bc35d
caorenmei <caorenmei@outlook.com>
2017-08-13 16:25:14 +0800

    mysql的查找脚本

---
+++ cmake/FindMysql.cmake
@@ -0,0 +1,22 @@
+# - Find Mysql library
+# Find the native Mysql includes and library
+#
+# MYSQL_INCLUDE_DIR - where to find mysqlclient/db.h, etc.
+# MYSQL_LIBRARIES - List of libraries when using mysqlclient/db.h.
+# MYSQL_FOUND - True if mysql found.
+
+find_path(MYSQL_INCLUDE_DIR
+  NAMES mysql.h
+  HINTS ${MYSQL_ROOT_DIR}/include ${CMAKE_INCLUDE_PATH})
+
+find_library(MYSQL_LIBRARY
+  NAMES mysqlclient
+  HINTS ${MYSQL_ROOT_DIR}/lib ${CMAKE_LIBRARY_PATH})
+  
+set(MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDE_DIR})
+set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(mysqlclient DEFAULT_MSG MYSQL_LIBRARIES MYSQL_INCLUDE_DIRS)
+
+mark_as_advanced(MYSQL_LIBRARIES MYSQL_INCLUDE_DIRS)
---
+++ cmake/FindMysqlCpp.cmake
@@ -0,0 +1,22 @@
+# - Find MysqlCpp library
+# Find the native MysqlCpp includes and library
+#
+# MYSQLCPP_INCLUDE_DIR - where to find mysqlcppconn-static/db.h, etc.
+# MYSQLCPP_LIBRARIES - List of libraries when using mysqlcppconn-static/db.h.
+# MYSQLCPP_FOUND - True if mysql-connector-cpp found.
+
+find_path(MYSQLCPP_INCLUDE_DIR
+  NAMES mysql_connection.h
+  HINTS ${MYSQLCPP_ROOT_DIR}/include ${CMAKE_INCLUDE_PATH})
+
+find_library(MYSQLCPP_LIBRARY
+  NAMES mysqlcppconn-static
+  HINTS ${MYSQLCPP_ROOT_DIR}/lib ${CMAKE_LIBRARY_PATH})
+  
+set(MYSQLCPP_INCLUDE_DIRS ${MYSQLCPP_INCLUDE_DIR})
+set(MYSQLCPP_LIBRARIES ${MYSQLCPP_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(mysqlcppconn-static DEFAULT_MSG MYSQLCPP_LIBRARIES MYSQLCPP_INCLUDE_DIRS)
+
+mark_as_advanced(MYSQLCPP_LIBRARIES MYSQLCPP_INCLUDE_DIRS)
