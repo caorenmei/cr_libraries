f4dc4c214892524fbcdc4513487a8213e9a4698d
caorenmei <caorenmei@outlook.com>
2017-11-08 17:35:42 +0800

    名字服务器状态机

--- src/common/cr/app/name_service.cpp
+++ src/common/cr/app/name_service.cpp
@@ -2,7 +2,9 @@
 
 #include <cassert>
 #include <ctime>
+#include <iomanip>
 #include <limits>
+#include <sstream>
 
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/uuid_generators.hpp>
@@ -14,30 +16,20 @@ namespace cr
         // 节点定义
         struct NameService::ValueNode
         {
-            // 创建类型
-            enum CreateMode
-            {
-                // 持久节点
-                PERSISTENT = 1,
-                // 持久持久顺序节点
-                PERSISTENT_SEQUENTIAL = 2,
-                // 临时节点
-                EPHEMERAL = 3,
-                // 临时顺序节点
-                EPHEMERAL_SEQUENTIAL = 4,
-            };
+            // 节点名
+            std::string name;
             // 父节点
             std::weak_ptr<ValueNode> parent;
-            // 版本
-            std::uint64_t version;
+            // 数据版本
+            std::uint64_t version = 0;
+            // 子节点版本
+            std::uint64_t cversion = 0;
             // 所属客户端
             boost::uuids::uuid owner;
             // 节点类型
-            CreateMode mode;
+            uint32_t mode = pb::PERSISTENT;
             // 序号
-            std::uint64_t sequential;
-            // 节点名
-            std::string name;
+            std::uint64_t sequential = 0;
             // 值
             std::string value;
             // 子节点
@@ -57,6 +49,19 @@ namespace cr
             std::map<boost::uuids::uuid, std::uint64_t> uuidMsgs;
         };
 
+        // 路径到字符串
+        static std::string pathToString(const std::vector<std::string>& strings)
+        {
+            std::string text = "[ ";
+            for (std::size_t i = 0; i != strings.size(); ++i)
+            {
+                text.append(strings[i]);
+                text.append(", ");
+            }
+            text.append("]");
+            return text;
+        }
+
         NameService::NameService(cr::app::Application& context, boost::asio::io_service& ioService,
             std::uint32_t id, std::string name, const Options& options)
             : RaftService(context, ioService, id, name, options),
@@ -64,6 +69,8 @@ namespace cr
             logger_(RaftService::getLogger()),
             options_(options),
             tickTimer_(ioService_),
+            versionIndex_(0),
+            root_(std::make_shared<ValueNode>()),
             acceptor_(ioService_),
             socket_(ioService_),
             nextId_(0)
@@ -85,9 +92,212 @@ namespace cr
         void NameService::onCommand(std::uint64_t index, const std::string& value)
         {
             CRLOG_DEBUG(logger_, "NameService") << "Handle Command: " << index << "->[length:" << value.size() << "]";
+            // 解析包
             std::string typeName = value.c_str();
             const char* data = value.data() + typeName.size() + 1;
             std::size_t dataLen = value.size() - (typeName.size() + 1);
+            // 序列化
+            if (typeName == pb::OpCommand::descriptor()->full_name())
+            {
+                pb::OpCommand command;
+                if (command.ParseFromArray(data, dataLen))
+                {
+                    onOpCommand(command);
+                }
+                else
+                {
+                    CRLOG_WARN(logger_, "NameService") << "Parse Command Failed: " << typeName;
+                }
+            }
+            else
+            {
+                CRLOG_WARN(logger_, "NameService") << "Unknow Command: " << typeName;
+            }
         }
+
+        void NameService::onOpCommand(const pb::OpCommand& command)
+        {
+            // 路径
+            if (command.path_size() == 0)
+            {
+                CRLOG_WARN(logger_, "NameService") << "Command Path Size: " << command.path_size();
+                return;
+            }
+            std::vector<std::string> path;
+            std::copy(command.path().begin(), command.path().end(), std::back_inserter(path));
+            // 客户端Id
+            boost::uuids::uuid clientId;
+            if (clientId.size() != command.client_id().size())
+            {
+                CRLOG_WARN(logger_, "NameService") << "Command Client Id Format Failed: " << command.client_id().size();
+                return;
+            }
+            std::memcpy(clientId.data, command.client_id().data(), clientId.size());
+            // 操作Id
+            boost::uuids::uuid commandId;
+            if (commandId.size() != command.client_id().size())
+            {
+                CRLOG_WARN(logger_, "NameService") << "Command Command Id Format Failed: " << command.command_id().size();
+                return;
+            }
+            std::memcpy(commandId.data, command.command_id().data(), commandId.size());
+            // 消息类型
+            switch (command.op())
+            {
+            case pb::ADD:
+                onAddCommand(clientId, commandId, path, command);
+                break;
+            case pb::UPDATE:
+                onUpdateCommand(clientId, commandId, path, command);
+                break;
+            case pb::REMOVE:
+                onRemoveCommand(clientId, commandId, path, command);
+                break;
+            default: break;
+            }
+        }
+
+        void NameService::onAddCommand(const boost::uuids::uuid& clientId, const boost::uuids::uuid& commandId,
+            const std::vector<std::string>& path, const pb::OpCommand& command)
+        {
+            auto parent = getParentValueNode(path);
+            // 父节点不存在
+            if (parent == nullptr)
+            {
+                CRLOG_WARN(logger_, "NameService") << "Add Parent Node Not Exists, Path = " << pathToString(path);
+                return;
+            }
+            // 子节点已存在
+            if (parent->children.count(path.back()) != 0)
+            {
+                CRLOG_WARN(logger_, "NameService") << "Add Node Already Exists, Path = " << pathToString(path);
+                return;
+            }
+            // 版本号不匹配
+            if (parent->cversion != command.version())
+            {
+                CRLOG_WARN(logger_, "NameService") << "Add Parent Node Version Mismatch, Version = " << parent->cversion << "!=" << command.version() << ", Path = " << pathToString(path);
+                return;
+            }
+            // 父节点是临时节点
+            if ((parent->mode & pb::EPHEMERAL) != 0)
+            {
+                CRLOG_WARN(logger_, "NameService") << "Add Parent Node Is Ephemeral Node, Version = " << parent->cversion << "!=" << command.version() << ", Path = " << pathToString(path);
+                return;
+            }
+            // 递增版本号
+            versionIndex_ = versionIndex_ + 1;
+            // 节点名字
+            std::string name = path.back();
+            if ((command.mode() & pb::SEQUENTIAL) != 0)
+            {
+                std::stringstream sstrm;
+                sstrm << std::setfill('0') << std::setw(10) << parent->sequential;
+                parent->sequential = parent->sequential + 1;
+                name += sstrm.str();
+            }
+            // 构造节点
+            auto node = std::make_shared<ValueNode>();
+            node->name = name;
+            node->parent = parent;
+            node->version = versionIndex_;
+            node->cversion = versionIndex_;
+            node->owner = clientId;
+            node->mode = command.mode();
+            node->value = command.value();
+            // 加入到父节点
+            parent->children.insert(std::make_pair(name, node));
+            parent->cversion = versionIndex_;
+            // 完成
+            CRLOG_DEBUG(logger_, "NameService") << "Add Node Success: " << pathToString(path);
+        }
+
+        void NameService::onUpdateCommand(const boost::uuids::uuid& clientId, const boost::uuids::uuid& commandId,
+            const std::vector<std::string>& path, const pb::OpCommand& command)
+        {
+            auto node = getValueNode(path);
+            // 节点不存在
+            if (node == nullptr)
+            {
+                CRLOG_WARN(logger_, "NameService") << "Update Node Not Exists, Path = " << pathToString(path);
+                return;
+            }
+            // 版本号不匹配
+            if (node->version != command.version())
+            {
+                CRLOG_WARN(logger_, "NameService") << "Update Node Version Mismatch, Version = " << node->version << "!=" << command.version() << ", Path = " << pathToString(path);
+                return;
+            }
+            // 递增版本号
+            versionIndex_ = versionIndex_ + 1;
+            // 更新
+            node->value = command.value();
+            node->version = versionIndex_;
+            // 完成
+            CRLOG_DEBUG(logger_, "NameService") << "Update Node Success: " << pathToString(path);
+        }
+
+        void NameService::onRemoveCommand(const boost::uuids::uuid& clientId, const boost::uuids::uuid& commandId,
+            const std::vector<std::string>& path, const pb::OpCommand& command)
+        {
+            auto node = getValueNode(path);
+            auto parent = node->parent.lock();
+            // 节点不存在
+            if (node == nullptr)
+            {
+                CRLOG_WARN(logger_, "NameService") << "Remove Node Not Exists, Path = " << pathToString(path);
+                return;
+            }
+            // 版本号不匹配
+            if (node->version != command.version())
+            {
+                CRLOG_WARN(logger_, "NameService") << "Remve Node Version Mismatch, Version = " << node->version << "!=" << command.version() << ", Path = " << pathToString(path);
+                return;
+            }
+            // 递增版本号
+            versionIndex_ = versionIndex_ + 1;
+            // 删除
+            parent->children.erase(node->name);
+            parent->cversion = versionIndex_;
+            // 完成
+            CRLOG_DEBUG(logger_, "NameService") << "Remove Node Success: " << pathToString(path);
+        }
+
+        std::shared_ptr<NameService::ValueNode> NameService::getValueNode(const std::vector<std::string>& path) const
+        {
+            std::shared_ptr<ValueNode> node = root_;
+            for (std::size_t depth = 0; depth < path.size() - 1 && node != nullptr; ++depth)
+            {
+                auto iter = node->children.find(path[depth]);
+                if (iter != node->children.end())
+                {
+                    node = iter->second;
+                }
+                else
+                {
+                    node = nullptr;
+                }
+            }
+            return node;
+        }
+
+        std::shared_ptr<NameService::ValueNode> NameService::getParentValueNode(const std::vector<std::string>& path) const
+        {
+            std::shared_ptr<ValueNode> parent = root_;
+            for (std::size_t depth = 0; depth < path.size() - 1 && parent != nullptr; ++depth)
+            {
+                auto iter = parent->children.find(path[depth]);
+                if (iter != parent->children.end())
+                {
+                    parent = iter->second;
+                }
+                else
+                {
+                    parent = nullptr;
+                }
+            }
+            return parent;
+        }
+
     }
 }
--- src/common/cr/app/name_service.h
+++ src/common/cr/app/name_service.h
@@ -59,6 +59,33 @@ namespace cr
 
         private:
 
+            // 节点数据结构
+            struct ValueNode;
+
+            // 连接
+            struct Connection;
+
+            // 操作指令
+            void onOpCommand(const pb::OpCommand& command);
+
+            // 添加指令
+            void onAddCommand(const boost::uuids::uuid& clientId, const boost::uuids::uuid& commandId, 
+                const std::vector<std::string>& path, const pb::OpCommand& command);
+
+            // 更新指令
+            void onUpdateCommand(const boost::uuids::uuid& clientId, const boost::uuids::uuid& commandId,
+                const std::vector<std::string>& path, const pb::OpCommand& command);
+
+            // 删除指令
+            void onRemoveCommand(const boost::uuids::uuid& clientId, const boost::uuids::uuid& commandId,
+                const std::vector<std::string>& path, const pb::OpCommand& command);
+
+            // 获取节点
+            std::shared_ptr<ValueNode> getValueNode(const std::vector<std::string>& path) const;
+
+            // 获取父节点
+            std::shared_ptr<ValueNode> getParentValueNode(const std::vector<std::string>& path) const;
+
             // io service
             boost::asio::io_service& ioService_;
             // 日志
@@ -67,12 +94,10 @@ namespace cr
             Options options_;
             // tick定时器
             boost::asio::steady_timer tickTimer_;
-            // 节点数据结构
-            struct ValueNode;
             // 下一个版本号
             std::uint64_t versionIndex_;
             // 节点树
-            std::map<std::string, std::shared_ptr<ValueNode>> tree_;
+            std::shared_ptr<ValueNode> root_;;
             // 临时节点
             std::map<boost::uuids::uuid, std::set<std::weak_ptr<ValueNode>>> ephemerals_;
             // 临时节点删除检测
@@ -82,12 +107,8 @@ namespace cr
             boost::asio::ip::tcp::socket socket_;
             // 序号
             std::uint64_t nextId_;
-            // 连接
-            struct Connection;
             // 客户端连接
             std::map<boost::uuids::uuid, std::shared_ptr<Connection>> connections_;
-            // uuid对应的客户端
-            std::map<boost::uuids::uuid, boost::uuids::uuid> uuids_;
         };
     }
 }
